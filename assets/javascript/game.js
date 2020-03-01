5db90b1c7cb8a38f631a2771ea36fd8e7564749f
Monica Nadal <missmonicanadal@gmail.com>
2020-02-29 16:41:23 -0800

    20200229 game is working end-to-end with the exception of only counting discarded letters once, but since i'm giving them half the alphabet to guess i think thats okay, and no hints

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,97 +1,129 @@
 $(document).ready(function () {
     console.log("ready!");
-    //on keyup log add element to the page 
 
     var guessCount = 0;
     var wrongGuess = "";
+    var randomWord = "";
+    var wordArray = [];
+    var gameWords = [
+        "avuncular", "defenestrate", "widdershins", "paraprosdokian", "schadenfreude", "hubris", "quixotic"
+    ];
+    var hashes = [];
+    var winsCount = 0;
+    var lossesCount = 0; 
+    var gameOver = false;
+
+    function init() {
+        guessCount = 0;
+        wrongGuess = "";
+        randomWord = "";
+        wordArray = [];
+        hashes = [];
+        gameOver = false;
+        $("#discardedLetter").text("Process of elimination!");
+        $("#guessCount").text("You have 12 guesses!");
+        $("#result").empty();
+        console.log("game over state: " + gameOver);
+    };
+    init();
+
+    function pickWord() {
+        var index = Math.floor(Math.random() * gameWords.length);
+        randomWord = gameWords[index];
+        hashes = [];
+        console.log("random word picked out of the random word array: ", randomWord);
+
+        //this for loop is making the hashes for the letters in the word dynamically 
+        for (let i = 0; i < randomWord.length; i++) {
+            hashes.push("_");
+            wordArray.push(randomWord[i]);
+        }
+        console.log("hashes in pickWord - ", hashes, wordArray)
+        $("#newWord").text(hashes.join(' '));
+
+    }
+    pickWord()
+    //don't change pickword - 
+
+
+
+    //re-initialize all of the other elements before calling pickword
+    $("#reset").on("click", function () {
+        console.log("You pressed reset!");
+        init();
+        pickWord();
+    })
+
+    //when reset button is pressed, call this function - call pickWord another time, and then the initializations
+    //only make button visible when the game is over - $().hide and $().show
+    //clear out the discarded 
+
+    function checkWord() {
+
+        var matches = true;
+
+        for (let i = 0; i < hashes.length; i++) {
+            if (hashes[i] !== wordArray[i]) {
+                matches = false;
+            }
+        }
+        if (matches) {
+            gameOver = true;
+            winsCount++
+            $("#result").text("You guessed it! Play again?");
+            $("#scoreWins").text("Wins: " + winsCount);
+            console.log("Wins: ", winsCount, gameOver);
+        }
+
+    }
 
     document.onkeyup = function (detectKey) {
         var userGuess = detectKey.key.toLowerCase();
         var userKey = event.keyCode;
-        console.log(userGuess);
+
+        console.log("what is user guess: ", userGuess);
+        if (gameOver === true){
+            return;
+        }
 
         if ((userKey > 64 && userKey < 91) || (userKey > 96 && userKey < 123)) {
+            if (randomWord.indexOf(userGuess) > -1) {
+                //this is checking if guess is right
+                for (let i = 0; i < randomWord.length; i++) {
+                    if (userGuess === randomWord[i]) {
+                        hashes[i] = userGuess;
+                    }
+                }
+            }
 
-            //if else for the word itself - 
-            if (userGuess === "p") {
-                $(".pGuess").text("p");
+            //checks on each correct guess if this is the game word 
+            var indexInWord = randomWord.indexOf(userGuess);
+            if (indexInWord > -1) {
 
-            } else if (userGuess === "a") {
-                $(".aGuess").text("a");
+                hashes[indexInWord] = userGuess;
+                $("#newWord").text(hashes.join(' '));
+                checkWord();
 
-            } else if (userGuess === "r") {
-                $(".rGuess").text("r");
-
-            } else if (userGuess === "o") {
-                $(".oGuess").text("o");
-
-            } else if (userGuess === "s") {
-                $(".sGuess").text("s");
-
-            } else if (userGuess === "d") {
-                $(".dGuess").text("d");
-
-            } else if (userGuess === "k") {
-                $(".kGuess").text("k");
-
-            } else if (userGuess === "i") {
-                $(".iGuess").text("i");
-
-            } else if (userGuess === "n") {
-                $(".nGuess").text("n");
-
-            } else if (userGuess !== "p" || userGuess !== "a" || userGuess !== "r" || userGuess !== "o" || userGuess !== "s" || userGuess !== "d" || userGuess !== "k" || userGuess !== "i" || userGuess !== "n") {
+            } else {
                 guessCount++;
-                wrongGuess += userGuess + " ";
+                wrongGuess += userGuess + ' ';
                 console.log(wrongGuess);
-                $("#discardedLetter").text("wrongGuess: " + wrongGuess);
 
-                // $("#guessCount").text("Incorrect guesses: " + guessCount);
+                $("#discardedLetter").text("Discarded letters: " + wrongGuess);
+
                 var guessesLeft = 12 - guessCount;
                 $("#guessCount").text("You have this many guesses left: " + guessesLeft);
                 console.log("This is the guess count: " + guessCount);
                 console.log("This is guesses left: " + guessesLeft);
                 if (guessesLeft === 0) {
-                    $(".wordContainer").text("Out of guesses!")
+                    $("#guessCount").text("Out of guesses!")
+                    lossesCount++
+                    $("#scoreLosses").text("Losses: " + lossesCount);
+                    console.log("Losses: ", lossesCount);
+                    gameOver = true;
                 }
-            };
-
+            }
         }
-        //if the user guesses a letter in the the word, reveal the letters in the word
-        //let's append it to the element with the class of that letter 
-
     }
-    //if the user has already picked an incorrect letter, tell them they've already guessed it?
-
-    // the word is "paraprosdokian"
-    // definition: (noun) a figure of speech in which the end of the sentence is surprising, or causes the reader to reinterpret the first part
-    // examples:
-    // Albert Einstein: “The difference between stupidity and genius is that genius has its limits.
-    // Douglas Adams: "Trin Tragula – for that was his name – was a dreamer, a thinker, a speculative philosopher or, as his wife would have it, an idiot…"
-    // Unattributed: "The last thing I want to do is hurt you. But it’s still on the list."
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
 
---
+++ assets/javascript/game_simple.js
@@ -0,0 +1,97 @@
+$(document).ready(function () {
+    console.log("ready!");
+    //on keyup log add element to the page 
+
+    var guessCount = 0;
+    var wrongGuess = "";
+
+    document.onkeyup = function (detectKey) {
+        var userGuess = detectKey.key.toLowerCase();
+        var userKey = event.keyCode;
+        console.log(userGuess);
+
+        if ((userKey > 64 && userKey < 91) || (userKey > 96 && userKey < 123)) {
+
+            //if else for the word itself - 
+            if (userGuess === "p") {
+                $(".pGuess").text("p");
+
+            } else if (userGuess === "a") {
+                $(".aGuess").text("a");
+
+            } else if (userGuess === "r") {
+                $(".rGuess").text("r");
+
+            } else if (userGuess === "o") {
+                $(".oGuess").text("o");
+
+            } else if (userGuess === "s") {
+                $(".sGuess").text("s");
+
+            } else if (userGuess === "d") {
+                $(".dGuess").text("d");
+
+            } else if (userGuess === "k") {
+                $(".kGuess").text("k");
+
+            } else if (userGuess === "i") {
+                $(".iGuess").text("i");
+
+            } else if (userGuess === "n") {
+                $(".nGuess").text("n");
+
+            } else if (userGuess !== "p" || userGuess !== "a" || userGuess !== "r" || userGuess !== "o" || userGuess !== "s" || userGuess !== "d" || userGuess !== "k" || userGuess !== "i" || userGuess !== "n") {
+                guessCount++;
+                wrongGuess += userGuess + " ";
+                console.log(wrongGuess);
+                $("#discardedLetter").text("Discarded Letters: " + wrongGuess);
+
+                // $("#guessCount").text("Incorrect guesses: " + guessCount);
+                var guessesLeft = 12 - guessCount;
+                $("#guessCount").text("You have this many guesses left: " + guessesLeft);
+                console.log("This is the guess count: " + guessCount);
+                console.log("This is guesses left: " + guessesLeft);
+                if (guessesLeft === 0) {
+                    $(".wordContainer").text("Out of guesses!")
+                }
+            };
+
+        }
+        //if the user guesses a letter in the the word, reveal the letters in the word
+        //let's append it to the element with the class of that letter 
+
+    }
+    //if the user has already picked an incorrect letter, tell them they've already guessed it?
+
+    // the word is "paraprosdokian"
+    // definition: (noun) a figure of speech in which the end of the sentence is surprising, or causes the reader to reinterpret the first part
+    // examples:
+    // Albert Einstein: “The difference between stupidity and genius is that genius has its limits.
+    // Douglas Adams: "Trin Tragula – for that was his name – was a dreamer, a thinker, a speculative philosopher or, as his wife would have it, an idiot…"
+    // Unattributed: "The last thing I want to do is hurt you. But it’s still on the list."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
